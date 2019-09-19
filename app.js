0a3554b26f2c98eed59e08dfc28c44bdf1b056ea
LBDCode <libby.duggan@gmail.com>
2019-09-19 11:27:44 -0400

    refactor checkWinner

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ $(document).ready(function() {
     $('#gameButtons').hide();
     $('#hintArea').hide();
     $('#gameArea').hide();
+    
     var game;
 
-    const Gameboard = function(disks = 5, pegs = 3) {
+    const Gameboard = function(disks, pegs) {
         this.disks = disks;
         this.pegs = pegs;
         this.board = [];
@@ -20,31 +21,34 @@ $(document).ready(function() {
             for(let j = d; j > 0; j--) {
                 this.board[0].push(j);
             }
+            
             this.winningSum = this.board[0].reduce((sum, pegAmount) => sum + pegAmount);
         };
 
         this.printBoard = function() {
             let curBoard = this.board.map(item => (`--- ${item.join(' ')}`));
             let boardHTML = curBoard.map(item => `<p>${item}</p>`);
+            
             return boardHTML;
         };
         
         this.checkMove = function(oldPegIndex, newPegIndex) {
             let movedDiskValue = this.board[oldPegIndex][this.board[oldPegIndex].length - 1];
             let currentTopDiskValue = this.board[newPegIndex][this.board[newPegIndex].length - 1];
+            
             return (currentTopDiskValue === undefined || currentTopDiskValue - movedDiskValue >= 0) ? true : false;
         };
 
         this.moveDisk = function(oldPeg, newPeg) {
             const oldIndex = oldPeg - 1;
             const newIndex = newPeg - 1;
+
             if(this.checkMove(oldIndex, newIndex)) {
                 this.board[newIndex].push(this.board[oldIndex][this.board[oldIndex].length - 1]);
                 this.board[oldIndex].pop();
                 this.moves++;
                 $('#board').html(this.printBoard());
                 $('#gameMessage').text('Make a move!');
-
             } else {
                 $('#gameMessage').text(`You cannot move a disk from peg ${oldPeg} to peg ${newPeg}. 
                   Try a different move, or ask for a hint!`);
@@ -52,22 +56,28 @@ $(document).ready(function() {
         }; 
 
         this.possibleMoves = function(peg) {
+            // refactor filter method
             const topDisk = this.board[peg - 1][this.board[peg - 1].length - 1];
             let movesMessage = '';
-            this.board.filter(function(p, index) {
-                if (topDisk === undefined) {
-                    movesMessage = `There are no disks on peg ${peg}.  No moves possible.`;
-                } else if (p.length === 0 || p[p.length - 1] > topDisk) {
-                    movesMessage += `Possible move: disk ${topDisk} from peg ${peg} to peg ${index + 1} <br>`;
-                } 
+            
+            let moves = this.board.filter(function(p, index) { 
+                return p.length > 0
+                // if (topDisk === undefined) {
+                //     movesMessage = `There are no disks on peg ${peg}.  No moves possible.`;
+                // } else if (p.length === 0 || p[p.length - 1] > topDisk) {
+                //     movesMessage += `Possible move: disk ${topDisk} from peg ${peg} to peg ${index + 1} <br>`;
+                // } 
             });
+            console.log(moves);
+            
             $('#gameMessage').html(movesMessage);
         };
 
         this.checkWinner = function() {
+            // refactored reduce method
             this.board.map(peg => {
-                if (peg.length > 0) {
-                    peg.reduce((sum, pegAmount) => sum + pegAmount) === this.winningSum ? this.win = true : null;
+                if (peg.length === this.disks) {
+                    this.win = peg.reduce((sum, pegAmount) => sum + pegAmount) === this.winningSum;
                 }
             });
         };
@@ -78,6 +88,7 @@ $(document).ready(function() {
             this.moves = 0;
             this.winningSum = 0;
             this.createBoard();
+            
             $('#board').html(this.printBoard());    
             $('#gameMessage').text('Make a move!');
         };
@@ -98,7 +109,6 @@ $(document).ready(function() {
             $('#gameArea').show();    
         } else {
             $('#welcomeMessage').text('Pegs and Disks values must be integers greater than 0.');
-
         }
     });
 
@@ -111,9 +121,9 @@ $(document).ready(function() {
             if (oldPeg.length < 1 || newPeg.length < 1) throw `
                 Please enter an original peg and new peg location.`;
             if (!(parseInt(oldPeg) >= 1 && parseInt(oldPeg) <= game.pegs)) throw `
-                Please enter an original peg number between 1 and ${game.pegs}`
+                Please enter an original peg number between 1 and ${game.pegs}`;
             if (!(parseInt(newPeg) >= 1 && parseInt(newPeg) <= game.pegs)) throw `
-                Please enter an original peg number between 1 and ${game.pegs}`   
+                Please enter an original peg number between 1 and ${game.pegs}`;   
 
             oldPeg = parseInt(oldPeg);
             newPeg = parseInt(newPeg);
@@ -138,7 +148,7 @@ $(document).ready(function() {
             if (hintPeg.length < 1) throw `
                 Please enter a hint peg.`;
             if (!(parseInt(hintPeg) >= 1 && parseInt(hintPeg) <= game.pegs)) throw `
-                Please enter an hint peg number between 1 and ${game.pegs}`
+                Please enter an hint peg number between 1 and ${game.pegs}.`;
 
             hintPeg = parseInt(hintPeg);
             game.possibleMoves(hintPeg);
